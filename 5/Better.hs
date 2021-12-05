47154d8ac9618f7a1b2d0f9fd4dc79dce7763bc0
Evan Rinehart <evanrinehart@gmail.com>
2021-12-05 01:06:48 -0600

    tidy

--- 5/Better.hs
+++ 5/Better.hs
@@ -12,16 +12,16 @@ import Data.Vector.Storable as V (thaw, freeze, fromListN, toList)
 import Data.Vector.Storable.Mutable as V (IOVector)
 import Data.Vector.Algorithms.AmericanFlag as V (sort)
 
+parseNum :: String -> (Int,String)
+parseNum xs =
+  let (ss, rest) = span isDigit xs in
+  (read ss, rest)
+
 parsePair :: String -> ((Int,Int),String)
 parsePair xs =
   let (a,rest) = parseNum xs in
   let (b,rest') = parseNum (drop 1 rest) in
   ((a,b),rest')
-
-parseNum :: String -> (Int,String)
-parseNum xs =
-  let (ss, rest) = span isDigit xs in
-  (read ss, rest)
 
 parseLine :: String -> Line
 parseLine xs =
@@ -31,15 +31,15 @@ parseLine xs =
 
 data Line = L Int Int Int Int deriving Show
 
-points :: Line -> [(Int,Int)]
-points l@(L a b c d) | a==c = map (a,) (range b d)
-                     | b==d = map (,b) (range a c)
-                     | otherwise = zip (range a c) (range b d)
-
 range :: Int -> Int -> [Int]
 range a b | a < b = [a..b]
           | b < a = [a,a-1..b]
           | otherwise = [a]
+
+points :: Line -> [(Int,Int)]
+points (L a b c d) | a==c = map (a,) (range b d)
+                   | b==d = map (,b) (range a c)
+                   | otherwise = zip (range a c) (range b d)
 
 encode :: (Int,Int) -> Int
 encode (x,y) = x*1000 + y
@@ -48,9 +48,7 @@ encodeLine :: Line -> [Int]
 encodeLine l = map encode (points l)
 
 main = do
-  putStrLn "start of program..."
-  entries <- fmap (map parseLine . lines) (readFile "input")
-  let everything = concatMap encodeLine entries
+  everything <- fmap (concatMap encodeLine . map parseLine . lines) (readFile "input")
   v1 <- V.thaw (V.fromListN 1000000 everything) :: IO (IOVector Int)
   V.sort v1 
   v2 <- V.freeze v1
