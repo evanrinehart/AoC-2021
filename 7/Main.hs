ea29664b5b33a64792e3b493a1eccebbaa2009f8
Evan Rinehart <evanrinehart@gmail.com>
2021-12-07 15:51:36 -0600

    checking some statistics

--- 7/Main.hs
+++ 7/Main.hs
@@ -1,9 +1,18 @@
 module Main where
 
 import Data.List
+import Data.Ord
 
 main = do
   ns <- getData
+  print ("median", median ns)
+  print ("avg", avg ns)
+  let hmm = map (\i -> (i,cost1 i ns)) [0..1850]
+  let hmm2 = fst (minimumBy (comparing snd) hmm)
+  print ("best location part 1", hmm2)
+  let hmm3 = map (\i -> (i,cost2 i ns)) [0..1850]
+  let hmm4 = fst (minimumBy (comparing snd) hmm3)
+  print ("best location part 2", hmm4)
   let costs1 = map (\i -> cost1 i ns) [0..1850]
   let costs2 = map (\i -> cost2 i ns) [0..1850]
   print (minimum costs1)
@@ -28,3 +37,7 @@ split s = go where
     (chunk,[])   -> [chunk]
     (chunk,more) -> chunk : go (dropOnly s more)
   dropOnly s y@(x:xs) = if x == s then xs else y
+
+
+avg xs = fromIntegral (sum xs) / fromIntegral (length xs)
+median xs = (sort xs) !! half where half = length xs `div` 2
