a100e7295b5633446250e7c9cf478606a5ab39ed
Evan Rinehart <evanrinehart@gmail.com>
2021-12-26 13:18:03 -0600

    tidy day 25

--- 25/Main.hs
+++ 25/Main.hs
@@ -2,81 +2,90 @@
 {- cabal:
   build-depends: base, vector
 -}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE DeriveFunctor #-}
 module Main where
 
-import Data.List
-import Data.Char
-import Data.Ord
-import Data.Maybe
-import Data.Monoid
-import Control.Monad
-import Control.Concurrent
---import qualified Data.Map.Strict as M; import Data.Map.Strict (Map, (!))
---import qualified Data.IntMap as IM; import Data.IntMap (IntMap)
---import qualified Data.Set as S hiding (split); import Data.Set (Set)
 import qualified Data.Vector as V
 import Data.Vector (Vector, (!))
-import Debug.Trace
 
--- a torus specifically with 137 "rows" and 139 "columns"
-data Torus a = Torus (Vector (Vector a)) Int Int deriving (Eq,Show,Functor)
+cuke :: Rules
+cuke _ _ _ E O _ _ _ _ = E
+cuke _ V _ _ O _ _ _ _ = V
+cuke _ V _ _ E O _ _ _ = V
+cuke _ _ _ _ E O _ _ _ = O
+cuke _ _ _ _ V _ _ E O = O
+cuke _ _ _ _ V _ O O _ = O
+cuke _ _ _ _ V _ V O _ = O
+cuke _ _ _ _ x _ _ _ _ = x
 
-torusH :: Torus a -> Int
-torusH (Torus rows _ _) = V.length rows
+data Fish = O | E | V deriving Eq
 
-torusW :: Torus a -> Int
-torusW (Torus rows _ _) = V.length (rows ! 0)
+type Rules = Fish -> Fish -> Fish -> Fish -> Fish -> Fish -> Fish -> Fish -> Fish -> Fish
+
+data Torus a = Torus (Vector (Vector a)) Int Int
+  deriving (Eq,Functor)
+
+torusWH :: Torus a -> (Int,Int)
+torusWH (Torus rows _ _) = (V.length (rows ! 0), V.length rows)
 
 readTorus :: Int -> Int -> Torus a -> a
 readTorus i j t@(Torus rows di dj) = (rows ! j') ! i' where
   i' = (i + di) `mod` w
   j' = (j + dj) `mod` h
-  w = V.length (rows ! 0)
-  h = V.length rows
+  (w,h) = torusWH t
 
-genTorus :: Int -> Int -> (Int -> Int -> a) -> Torus a
-genTorus w h g = Torus (V.generate h (\j -> V.generate w (\i -> g i j))) 0 0
+rotateTorus :: Torus a -> Int -> Int -> Torus a
+rotateTorus (Torus rows di dj) dx dy = Torus rows (di + dx) (dj + dy)
 
 duplicateTorus :: Torus a -> Torus (Torus a)
-duplicateTorus (Torus rows di dj) = genTorus w h (\i j -> Torus rows (di+i) (dj+j)) where
-  w = V.length (rows ! 0)
-  h = V.length rows
+duplicateTorus t = genTorus (torusWH t) (rotateTorus t)
 
-foldlTorus :: (Int -> Int -> b -> a -> b) -> b -> Torus a -> b
-foldlTorus f b0 (Torus rows _ _) = V.ifoldl' g b0 rows where
-  g rowb j row = V.ifoldl' (\b i x -> f i j b x) rowb row
+genTorus :: (Int,Int) -> (Int -> Int -> a) -> Torus a
+genTorus (w,h) g = Torus (V.generate h (\j -> V.generate w (\i -> g i j))) 0 0
 
-type Rules = Fish -> Fish -> Fish -> Fish -> Fish -> Fish -> Fish -> Fish -> Fish -> Fish
-data Fish = O | E | V deriving (Eq,Show)
+instance Show a => Show (Torus a) where
+  show = pprint
 
-rowToString row = map f (V.toList row) where
-  f O = '.'
-  f E = '>'
-  f V = 'v'
+instance Show Fish where
+  show c = [f2c c]
 
-pprint (Torus rows _ _) = mapM_ (\row -> putStrLn (rowToString row)) rows
+
+-- parser
+
+getData = do
+  ls <- fmap lines (readFile "input")
+  let rows = V.fromList (map (V.fromList . parseLine) ls)
+  return (Torus rows 0 0)
+
+parseLine :: String -> [Fish]
+parseLine = map c2fish
 
 c2fish '.' = O
 c2fish '>' = E
 c2fish 'v' = V
 
-val O = 0
-val E = 1
-val V = 1000
+-- pretty printer
 
-checksum = foldlTorus (\i j b x -> b + val x * j + i) 0
+pprint :: Show a => Torus a -> String
+pprint (Torus rows _ _) = (unlines . map (\row -> concatMap show (V.toList row)) . V.toList) rows
 
-p1 :: Rules
-p1 _ _ _ E O _ _ _ _ = E
-p1 _ V _ _ O _ _ _ _ = V
-p1 _ V _ _ E O _ _ _ = V
-p1 _ _ _ _ E O _ _ _ = O
-p1 _ _ _ _ V _ _ E O = O
-p1 _ _ _ _ V _ O O _ = O
-p1 _ _ _ _ V _ V O _ = O
-p1 _ _ _ _ x _ _ _ _ = x
+f2c O = '.'
+f2c E = '>'
+f2c V = 'v'
+
+-- example
+
+ex :: Torus Fish
+ex = Torus (V.fromList
+  [V.fromList [V,O,O,O]
+  ,V.fromList [O,O,O,O]
+  ,V.fromList [E,E,E,O]
+  ,V.fromList [O,O,V,O]]) 0 0
+
+
+
+
+-- Apply rules to torus of fish
 
 step :: Rules -> Torus Fish -> Torus Fish
 step rules tbase = fmap g (duplicateTorus tbase) where
@@ -91,27 +100,12 @@ step rules tbase = fmap g (duplicateTorus tbase) where
     (readTorus 0 1 t) 
     (readTorus 1 1 t) 
 
-ex :: Torus Fish
-ex = Torus (V.fromList
-  [V.fromList [V,O,O,O]
-  ,V.fromList [O,O,O,O]
-  ,V.fromList [E,E,E,O]
-  ,V.fromList [O,O,V,O]]) 0 0
-
+search :: Int -> Torus Fish -> Int
+search n t =
+  let t' = step cuke t in
+  if t==t'
+    then n+1
+    else search (n+1) t'
 
 main :: IO ()
-main = do 
-  t <- getData
-  let final = search 0 t
-  print final
-
-search :: Int -> Torus Fish -> Int
-search n t = let t' = step p1 t in if t==t' then n+1 else search (n+1) t'
-
-getData = do
-  ls <- fmap lines (readFile "input")
-  let rows = V.fromList (map (V.fromList . parseLine) ls)
-  return (Torus rows 0 0)
-
-parseLine :: String -> [Fish]
-parseLine = map c2fish
+main = print =<< pure . search 0 =<< getData
